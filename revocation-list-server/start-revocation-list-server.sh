19a2edea2ab053f59f59b523b30d6508681c12d4
George Fourtounis <gfour@admin.grnet.gr>
2026-09-03 11:41:52 +0000

    Support IP configuration

--- revocation-list-server/start-revocation-list-server.sh
+++ revocation-list-server/start-revocation-list-server.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-IP=$(hostname -I | awk '{print $1}')
+if [[ -f ".config.ip" ]]; then
+    IP=$(cat .config.ip)
+else
+    IP=$(hostname -I | awk '{print $1}')
+fi
+
 PORT=5607
 set -x
 python3 -m http.server ${PORT} --bind ${IP} -d crl
